0e03d284d9488829d8841e192675799534f7a06c
Camiloaa <juancaa1212@gmail.com>
2025-05-11 21:19:57 -0500

    prueba

--- controller/horoscopoController.js
+++ controller/horoscopoController.js
@@ -5,19 +5,23 @@ export default class HoroscopoController {
     }
   
     init() {
-      this.view.onFechaCambiada((valido) => {
-        this.view.habilitarBoton(valido);
-      });
-  
-      this.view.onClickConsultar((fecha) => {
-        const signo = this.model.obtenerSigno(fecha);
-        if (signo) {
-          const horoscopo = this.model.obtenerHoroscopo(signo);
-          this.view.mostrarMensaje(horoscopo);
-        } else {
-          this.view.mostrarMensaje("Fecha inválida o signo no encontrado.");
-        }
-      });
+  this.view.onFechaCambiada((valido) => {
+    this.view.habilitarBoton(valido);
+  });
+
+  this.view.onClickConsultar((fecha) => {
+    const signo = this.model.obtenerSigno(fecha);
+    if (signo) {
+      const horoscopo = this.model.obtenerHoroscopo(signo);
+      this.view.mostrarMensaje(horoscopo);
+    } else {
+      this.view.mostrarMensaje("Fecha inválida o signo no encontrado.");
     }
-  }
-  
+  });
+
+  // Validar si el campo ya tiene una fecha válida al cargar
+  const fechaActual = this.view.inputFecha.value;
+  const valido = /^\d{2}-\d{2}-\d{4}$/.test(fechaActual);
+  this.view.habilitarBoton(valido);
+    }
+}
