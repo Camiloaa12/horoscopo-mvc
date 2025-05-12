885ca0f78be74c135b2dbb8760ba1418d50c8d86
Camiloaa <juancaa1212@gmail.com>
2025-05-11 21:14:17 -0500

    prueba

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import HoroscopoController from './controller/horoscopoController.js';
 import HoroscopoModel from './model/horoscopoModel.js';
 import HoroscopoView from './view/horoscopoView.js';
 
-const model = new HoroscopoModel();
-const view = new HoroscopoView();
-const controller = new HoroscopoController(model, view);
+document.addEventListener('DOMContentLoaded', () => {
+  const model = new HoroscopoModel();
+  const view = new HoroscopoView();
+  const controller = new HoroscopoController(model, view);
+
+  controller.init();
+});
